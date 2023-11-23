1c0a4b47b50e7605e9859244d76da0a4095cb20c
Diana Hilleshein <chibizova.diana@gmail.com>
2023-11-23 13:16:50 +0000

    Week 2

--- week2/search.py
+++ week2/search.py
@@ -63,9 +63,24 @@ def autocomplete():
         print(f"Prefix: {prefix}")
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
-            ##### W2, L3, S1
-            search_response = None
-            print("TODO: implement autocomplete AND instant search")
+            suggest_body = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion": {
+                            "field": "suggest",
+                            "size": 5,
+                            "skip_duplicates": True
+                        }
+                    }
+                }
+            }
+
+            index = "bbuy_queries" if type == "queries" else "bbuy_products"
+
+            opensearch = get_opensearch()
+
+            search_response = opensearch.search(index=index, body=suggest_body)
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -105,9 +120,7 @@ def query():
             explain = True
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
-        ##### W2, L1, S2
-
-        ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj=query_obj, user_query=user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -120,18 +133,16 @@ def query():
         if filters_input:
             (filters, display_filters, applied_filters) = process_filters(filters_input)
         query_obj = qu.create_query(user_query,  filters, sort, sortDir, size=20)
-        #### W2, L1, S2
-
-        ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj=query_obj, user_query=user_query)
 
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
-    #print("query obj: {}".format(query_obj))
+    print("query obj: {}".format(query_obj))
     response = opensearch.search(body=query_obj, index="bbuy_products", explain=explain)
     # Postprocess results here if you so desire
 
-    #print(response)
+    print(response)
     if error is None:
         return render_template("search_results.jinja2", query=user_query, search_response=response,
                                display_filters=display_filters, applied_filters=applied_filters,
